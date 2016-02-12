fa1e88e522436c5690e5ccfdf767e3fad0a3c885
Madeleine Walstad <madeleinewalstad@gmail.com>
2016-02-12 13:00:21 -0600

    add model for Lobbies

--- virtualboard/web/models.py
+++ virtualboard/web/models.py
@@ -5,3 +5,10 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.ForeignKey(User)
     motto = models.CharField(max_length=200, blank=True)
+
+class Lobby(models.Model):
+	name = models.CharField(max_length=30)
+	num_members = models.PositiveIntegerField()
+
+	def __unicode__(self):
+		return self.name
