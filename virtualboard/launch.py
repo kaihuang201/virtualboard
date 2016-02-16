7340b75be15c6c8a9deda6fd4611275500d492cf
Daniel Pugliese <dpugliese@Daniels-MacBook-Pro.local>
2016-02-15 23:06:43 -0500

    Fixing issue launch.py had with laoding static files

--- virtualboard/launch.py
+++ virtualboard/launch.py
@@ -12,11 +12,11 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'virtualboard.settings'
 def main():
     wsgi_app = tornado.wsgi.WSGIContainer(get_wsgi_application())
     settings = dict(
-            template_path=os.path.join(os.path.dirname(__file__), "views"),
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
-            xsrf_cookies=False,
-            debug=True,
-            cookie_secret=")r&u2_gbw4%wiyrv!7#6u0a-_axtp!i5j=q*ph-)p))yn_dk61",
+            template_path=os.path.join(os.path.dirname(__file__), "web/views"),
+            static_path=os.path.join(os.path.dirname(__file__), "web/static"),
+            #xsrf_cookies=False,
+            #debug=True,
+            #cookie_secret=")r&u2_gbw4%wiyrv!7#6u0a-_axtp!i5j=q*ph-)p))yn_dk61",
         )
     tornado_app = tornado.web.Application(
         [
--- virtualboard/web/tornado_handlers.py
+++ virtualboard/web/tornado_handlers.py
@@ -4,8 +4,44 @@ import json
 import logging
 import tornado.websocket
  
+from tornado import gen
 from django.utils.timezone import utc
+from web.modules.message_buffer import MessageBuffer
+
+global_message_buffer = MessageBuffer()
  
 class HelloHandler(tornado.web.RequestHandler):
   def get(self):
     self.write('Hello from tornado')
+
+class MessageNewHandler(tornado.web.RequestHandler):
+    def post(self):
+        message = {
+            "id": str(uuid.uuid4()),
+            "body": self.get_argument("body"),
+        }
+        # to_basestring is necessary for Python 3's json encoder,
+        # which doesn't accept byte strings.
+        message["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=message))
+        if self.get_argument("next", None):
+            self.redirect(self.get_argument("next"))
+        else:
+            self.write(message)
+        global_message_buffer.new_messages([message])
+
+
+class MessageUpdatesHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def post(self):
+        cursor = self.get_argument("cursor", None)
+        # Save the future returned by wait_for_messages so we can cancel
+        # it in wait_for_messages
+        self.future = global_message_buffer.wait_for_messages(cursor=cursor)
+        messages = yield self.future
+        if self.request.connection.stream.closed():
+            return
+        self.write(dict(messages=messages))
+
+    def on_connection_close(self):
+        global_message_buffer.cancel_wait(self.future)
