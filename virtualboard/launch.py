53cbfa06b3f70a48e375d12a581d1afefa7b5f95
Daniel Pugliese <dpugliese@Daniels-MacBook-Pro.local>
2016-02-15 22:29:56 -0500

    Added a new Python script that when run starts a Tornado server which can handle Tornado requests normally, but passes any unrecognized requests along to the Django project

---
+++ virtualboard/launch.py
@@ -0,0 +1,32 @@
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.wsgi
+import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+from tornado.options import options, define, parse_command_line
+from web.tornado_sockets import HelloHandler
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'virtualboard.settings'
+
+def main():
+    wsgi_app = tornado.wsgi.WSGIContainer(get_wsgi_application())
+    settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "views"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=False,
+            debug=True,
+            cookie_secret=")r&u2_gbw4%wiyrv!7#6u0a-_axtp!i5j=q*ph-)p))yn_dk61",
+        )
+    tornado_app = tornado.web.Application(
+        [
+            ('/hello/', HelloHandler),
+            ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
+        ], **settings)
+    tornado.options.parse_command_line()
+    server = tornado.httpserver.HTTPServer(tornado_app)
+    server.listen(8000)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
