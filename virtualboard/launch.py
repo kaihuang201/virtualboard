15639f123fa76536d07875900743a6682075343f
Daniel Pugliese <dpugliese@wirelessprvnat-172-17-247-71.near.illinois.edu>
2016-02-17 15:48:00 -0600

    Tweaked some urls and turned xsrf_cookies off for django so signin will work

--- virtualboard/launch.py
+++ virtualboard/launch.py
@@ -5,7 +5,7 @@ import tornado.wsgi
 import django.core.handlers.wsgi
 from django.core.wsgi import get_wsgi_application
 from tornado.options import options, define, parse_command_line
-from web.tornado_handlers import HelloHandler
+from web.tornado_handlers import *
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'virtualboard.settings'
 
@@ -14,13 +14,15 @@ def main():
     settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "web/views"),
             static_path=os.path.join(os.path.dirname(__file__), "web/static"),
-            #xsrf_cookies=False,
+            xsrf_cookies=False,
             #debug=True,
             #cookie_secret=")r&u2_gbw4%wiyrv!7#6u0a-_axtp!i5j=q*ph-)p))yn_dk61",
         )
     tornado_app = tornado.web.Application(
         [
-            ('/hello/', HelloHandler),
+            (r'/hello/$', HelloHandler),
+            (r'/a/message/new/$', MessageNewHandler),
+            (r'/a/message/updates/$', MessageUpdatesHandler),
             ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
         ], **settings)
     tornado.options.parse_command_line()
