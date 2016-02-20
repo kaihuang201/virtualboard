bffc3a86898a21049e967b7e274f32376d07eef3
Matt Hoffman <hoff121324@gmail.com>
2016-02-19 22:29:42 -0600

    fixed problems caused by adding camera rotation, added some pre-built piece definitions

--- virtualboard/src/main/js/vboard.js
+++ virtualboard/src/main/js/vboard.js
@@ -3,18 +3,25 @@ var VBoard = VBoard || {};
 	//size of the vertical view plane
 	vb.size = 10;
 
-	vb.javascriptInit = function() {
+	vb.javascriptInit = function () {
 		vb.simTime = Date.now();
 		window.addEventListener("resize", vb.setCameraPerspective);
 		window.addEventListener("keydown", function (evt) {
 			vb.inputs.onKeyDown(evt.keyCode);
+			if(!evt.metaKey) {
+				evt.preventDefault();
+			}
 		});
+
 		window.addEventListener("keyup", function (evt) {
 			vb.inputs.onKeyUp(evt.keyCode);
 		});
+
 		window.addEventListener("mousewheel", function (evt) {
 			vb.inputs.onScroll(Math.max(-1, Math.min(1, (evt.wheelDelta))));
 		});
+
+		//silly firefox doing firefox things
 		window.addEventListener("DOMMouseScroll", function (evt) {
 			vb.inputs.onScroll(Math.max(-1, Math.min(1, (-evt.detail))));
 		});
@@ -22,24 +29,80 @@ var VBoard = VBoard || {};
 		vb.loadDummyBlocks();
 	};
 
-	vb.board = (function() {
+	vb.board = (function () {
 
 		var board = {};
-		board.pieces = [];
+		board.pieces = []; //ordered list
 
-		board.add = function(piece) {
+		//this should probably be fetched as a separate file
+		board.pieceMap = {
+			"chessKingBlack" : {
+				"size" : 2.0,
+				"icon" : "nbking.png"
+			},
+			"chessQueenBlack" : {
+				"size" : 2.0,
+				"icon" : "nbqueen.png"
+			},
+			"chessKnightBlack" : {
+				"size" : 2.0,
+				"icon" : "nbknight.png"
+			},
+			"chessBishopBlack" : {
+				"size" : 2.0,
+				"icon" : "nbbishop.png"
+			},
+			"chessRookBlack" : {
+				"size" : 2.0,
+				"icon" : "nbrook.png"
+			},
+			"chessPawnBlack" : {
+				"size" : 2.0,
+				"icon" : "nbpawn.png"
+			},
+			"chessKingWhite" : {
+				"size" : 2.0,
+				"icon" : "nwking.png"
+			},
+			"chessQueenWhite" : {
+				"size" : 2.0,
+				"icon" : "nwqueen.png"
+			},
+			"chessKnightWhite" : {
+				"size" : 2.0,
+				"icon" : "nwknight.png"
+			},
+			"chessBishopWhite" : {
+				"size" : 2.0,
+				"icon" : "nwbishop.png"
+			},
+			"chessRookWhite" : {
+				"size" : 2.0,
+				"icon" : "nwrook.png"
+			},
+			"chessPawnWhite" : {
+				"size" : 2.0,
+				"icon" : "nwpawn.png"
+			},
+			"chessBoard" : {
+				"size" : 16.0,
+				"icon" : "background.png"
+			}
+		};
+
+		board.add = function (piece) {
 			board.pieces.push(piece);
 			var z = board.getZIndex(board.pieces.length-1);
 			piece.mesh.position.z = z;
 		};
 
-		board.getZIndex = function(index){
+		board.getZIndex = function (index) {
 			return 1 + (10/(0.2*index + 1));
 		};
 
-		board.remove = function(piece){
+		board.remove = function (piece) {
 			var index = board.pieces.indexOf(piece);
-			for(var i = index; i < board.pieces.length-1; i++){
+			for(var i = index; i < board.pieces.length-1; i++) {
 				board.pieces[i] = board.pieces[i+1];
 				board.pieces[i].mesh.position.z = board.getZIndex(i);
 			}
@@ -47,9 +110,9 @@ var VBoard = VBoard || {};
 			piece.mesh.dispose();
 		};
 
-		board.pushToBack = function(piece) {
+		board.pushToBack = function (piece) {
 			var index = board.pieces.indexOf(piece);
-			for(var i = index-1; i < 0; i--){
+			for(var i = index-1; i < 0; i--) {
 				board.pieces[i] = board.pieces[i-1];
 				board.pieces[i].mesh.position.z = board.getZIndex(i);
 			}
@@ -57,7 +120,7 @@ var VBoard = VBoard || {};
 			piece.mesh.position.z = board.getZIndex(0);
 		};
 
-		board.bringToFront = function(piece) {
+		board.bringToFront = function (piece) {
 			var index = board.pieces.indexOf(piece);
 			for(var i = index; i < board.pieces.length-1; i++) {
 				board.pieces[i] = board.pieces[i+1];
@@ -67,13 +130,20 @@ var VBoard = VBoard || {};
 			piece.mesh.position.z = board.getZIndex(board.pieces.length-1);
 		};
 
-		board.generateNewPiece = function(name, user, pos) {
+		board.generateNewPiece = function (name, user, pos) {
 
 			var material = new BABYLON.StandardMaterial("std", vb.scene);
-			material.diffuseTexture = new BABYLON.Texture("crown.png", vb.scene);
+			var icon = "crown.png";
+			var size = 3.0;
+
+			if(board.pieceMap.hasOwnProperty(name)) {
+				icon = board.pieceMap[name].icon;
+				size = board.pieceMap[name].size;
+			}
+			material.diffuseTexture = new BABYLON.Texture(icon, vb.scene);
 			material.diffuseTexture.hasAlpha = true;
 
-			var plane = BABYLON.Mesh.CreatePlane("plane", 3.0, vb.scene);
+			var plane = BABYLON.Mesh.CreatePlane("plane", size, vb.scene);
 			plane.material = material;
 			plane.position = new BABYLON.Vector3(pos.x, pos.y, 0);
 
@@ -83,32 +153,26 @@ var VBoard = VBoard || {};
 			piece.position = pos;
 			piece.pickedUp = !!user;
 			piece.mesh = plane;
-			piece.icon = "/../../../res/crown.png";
+			piece.icon = icon;
 
 			board.add(piece);
-
 		};
 
-		board.movePiece = function(piece, pos, user, instant) {
-
+		board.movePiece = function (piece, pos, user, instant) {
+			//to do
 		};
 
-		board.clearBoard = function() {
+		board.clearBoard = function () {
 			while(board.pieces.length > 0) {
 				board.remove(board.pieces[board.pieces.length-1]);
 			}
 		};
 
-		//board.getScreenCenter = function() {
-		//	return board.screenToGameSpace({
-		//		x: vb.canvas.width/2,
-		//		y: vb.canvas.height/2
-		//	});
-		//};
+		board.getCenter = function () {
+			return new BABYLON.Vector2(0, 0);
+		};
 
 		board.screenToGameSpace = function (position) {
-			//to do: adjust based on current camera orientation
-
 			//screen space
 			//also equals to the camera coordinates
 			var halfWidth = vb.canvas.width / 2;
@@ -118,96 +182,98 @@ var VBoard = VBoard || {};
 			var cameraX = vb.camera.position.x;
 			var cameraY = vb.camera.position.y;
 
+			//screen space
+			var screenDX = (position.x - halfWidth);
+			var screenDY = (position.y - halfHeight);
+
 			var ratio = vb.canvas.width / vb.canvas.height;
 			var size = vb.size;
 
+			var upX = vb.camera.upVector.x;
+			var upY = vb.camera.upVector.y;
 			var heightRatio = -size / halfHeight;
 			var widthRatio = ratio*size / halfWidth;
 
 			//game space
-			var dx = (position.x - halfWidth)*widthRatio;
-			var dy = (position.y - halfHeight)*heightRatio;
+			var dx = (screenDX*upY - screenDY*upX)*widthRatio;
+			var dy = (screenDY*upY + screenDX*upX)*heightRatio;
 			var totalX = cameraX + dx;
 			var totalY = cameraY + dy;
 
 			return new BABYLON.Vector2(totalX, totalY);
 		};
-
 		return board;
-
 	})();
 
-	vb.users = (function(){
+	vb.users = (function () {
 
 		var users = {};
 		users.local = null;
 		users.host = null;
-		users.userList = [];
+		users.userList = []; //unordered list
 
-		users.add = function(user){
+		users.add = function (user) {
 			users.userList.push(user);
 		};
 
-		users.remove = function(user){
+		users.remove = function (user) {
 			var index = users.userList.indexOf(user);
 
 			users.userList[index] = users.userList[users.userList.length-1];
 			users.userList.pop();
-
-
 		};
 
-		users.setLocal = function(user){
+		users.setLocal = function (user) {
 			users.local = user;
 		};
 
-		users.getLocal = function(){
+		users.getLocal = function () {
 			return users.local;
 		};
 
-		users.getNone = function(){
+		users.getNone = function () {
 			return null;
 		};
 
-		users.getHost = function(){
+		users.getHost = function () {
 			return users.host;
 		};
 
-		users.createNewUser = function(name, color){
+		users.createNewUser = function (name, color) {
 			var user = {};
 			user.name = name;
 			user.color = color;
 			user.ping = -1;
 			users.add(user);
 
-			if(!users.getLocal()){
+			if(!users.getLocal()) {
 				users.setLocal(user);
 			}
 		};
-
 		return users;
-
 	})();
 
-	vb.inputs = (function() {
+	vb.inputs = (function () {
 		var inputs = {};
 		inputs.keysPressed = [];
 
-		inputs.onKeyDown = function(key) {
-			console.log("NEW KEY: " + key);
+		inputs.onKeyDown = function (key) {
+			//console.log("NEW KEY: " + key);
 			if(inputs.keysPressed.indexOf(key) == -1) {
-				console.log("sanity check: " + key);
+				//console.log("sanity check: " + key);
 				inputs.keysPressed.push(key);
 			}
 		};
 
-		inputs.onKeyUp = function(key) {
+		inputs.onKeyUp = function (key) {
 			var index = inputs.keysPressed.indexOf(key);
 			inputs.keysPressed.splice(index, 1);
 		};
 
 		inputs.onScroll = function (delta) {
-			console.log(delta);
+			//console.log(delta);
+			//note: there is definitely some way for this to get messed up after the board has been manipulated to some extent
+			//it seems to have something to do with opening/closing the developer console
 
 			var mousePos = vb.board.screenToGameSpace({
 				x: vb.scene.pointerX,
@@ -232,40 +298,54 @@ var VBoard = VBoard || {};
 			vb.setCameraPerspective();
 		};
 
-		inputs.processInputs = function(elapsed) {
+		inputs.processInputs = function (elapsed) {
 			//console.log("ELAPSED: " + elapsed);
 			var inertia = 2000;
+			var up = vb.camera.upVector;
+			var dist = (elapsed / inertia) * vb.size
+
 			for(var index = 0; index < inputs.keysPressed.length; index++) {
 				switch(inputs.keysPressed[index]) {
 					case 87: //w
 					case 38: //up
-						//VBoard.verticalMoveLoop(1);
-						vb.camera.position.y += (elapsed / inertia)*vb.size;
+						vb.camera.position.y += dist * up.y;
+						vb.camera.position.x += dist * up.x;
 						break;
 					case 83: //s
 					case 40: //down
-						vb.camera.position.y -= (elapsed / inertia)*vb.size;
+						vb.camera.position.y -= dist * up.y;
+						vb.camera.position.x -= dist * up.x;
 						break;
 					case 68: //d
 					case 39: //right
-						vb.camera.position.x += (elapsed / inertia)*vb.size;
+						vb.camera.position.y -= dist * up.x;
+						vb.camera.position.x += dist * up.y;
 						break;
 					case 65: //a
 					case 37: //left
-						vb.camera.position.x -= (elapsed / inertia)*vb.size;
+						vb.camera.position.y += dist * up.x;
+						vb.camera.position.x -= dist * up.y;
 						break;
 					case 81: //q
 						//warning: breaks camera panning and screenToGameSpace
-						var up = vb.camera.upVector;
 						var upX = up.x*Math.cos(-elapsed/inertia) - up.y * Math.sin(-elapsed/inertia);
 						var upY = up.x*Math.sin(-elapsed/inertia) + up.y * Math.cos(-elapsed/inertia);
 						vb.camera.upVector = new BABYLON.Vector3(upX, upY, 0);
 						break;
 					case 69: //e
-						var up = vb.camera.upVector;
 						var upX = up.x*Math.cos(elapsed/inertia) - up.y * Math.sin(elapsed/inertia);
 						var upY = up.x*Math.sin(elapsed/inertia) + up.y * Math.cos(elapsed/inertia);
 						vb.camera.upVector = new BABYLON.Vector3(upX, upY, 0);
+						break;
+					case 8: //backspace
+						//resets the camera to default
+						//or at least it would in theory if backspace didn't also go back a page
+						vb.camera.position.x = 0;
+						vb.camera.position.y = 0;
+						vb.camera.upVector.x = 0;
+						vb.camera.upVector.y = 1;
+						vb.size = 10; //may need to be updated in the future
+						vb.setCameraPerspective();
 						break;
 				}
 			}
@@ -273,7 +353,7 @@ var VBoard = VBoard || {};
 		return inputs;
 	})();
 
-	vb.renderInit = function() {
+	vb.renderInit = function () {
 		vb.frame = 0;
 		vb.canvas = document.getElementById("canvas");
 		var canvas = vb.canvas;
@@ -305,7 +385,7 @@ var VBoard = VBoard || {};
 			scene.activeCamera = camera;
 			camera.detachControl(canvas);
 			vb.light = new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(0, 1, 0), scene);
-			vb.light.groundColor = new BABYLON.Color3(1, 1, 0.984);
+			vb.light.groundColor = new BABYLON.Color3(1, 1, 1);
 
 			return scene;
 		})();
@@ -338,243 +418,3 @@ $(document).ready(function () {
 	console.log("document ready");
 	VBoard.javascriptInit();
 });
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-	VBoard.javascriptInit = function () {
-		console.log("javascriptinit");
-		window.addEventListener("resize", VBoard.resizeFunc);
-		window.addEventListener("keydown", function (evt) {
-			switch(evt.keyCode) {
-				case 87: //w
-				case 38: //up
-					VBoard.verticalMoveLoop(1);
-					break;
-				case 83: //s
-				case 40: //down
-					VBoard.verticalMoveLoop(-1);
-					break;
-				case 68: //d
-				case 39: //right
-					VBoard.horizontalMoveLoop(1);
-					break;
-				case 65: //a
-				case 37: //left
-					VBoard.horizontalMoveLoop(-1);
-					break;
-			}
-		});
-		window.addEventListener("keyup", function (evt) {
-			switch(evt.keyCode) {
-				case 87: //w
-				case 38: //up
-					VBoard.verticalMoveLoop(0);
-					break;
-				case 83: //s
-				case 40: //down
-					VBoard.verticalMoveLoop(0);
-					break;
-				case 68: //d
-				case 39: //right
-					VBoard.horizontalMoveLoop(0);
-					break;
-				case 65: //a
-				case 37: //left
-					VBoard.horizontalMoveLoop(0);
-					break;
-			}
-		});
-		VBoard.renderInit();
-		VBoard.loadDummyBlocks();
-		var canvas = VBoard.engine.getRenderingCanvas();
-		//canvas.addEventListener("mousedown", function (evt) {
-		//	//if(evt.button !== 0) {
-		//	//	return;
-		//	// }
-		//	console.log("pointer down");
-		//
-		//	var pickInfo = VBoard.scene.pick(VBoard.scene.pointerX, VBoard.scene.pointerY, function (mesh) {
-		//		return true;
-		//	}, false);
-		//
-		//	if(pickInfo.hit) {
-		//		VBoard.currentMesh = pickInfo.pickedMesh;
-		//		VBoard.lastMeshPos = [VBoard.scene.pointerX, VBoard.scene.pointerY];
-		//	}
-		//});
-		canvas.addEventListener("mousedown", VBoard.onPointerDown);
-		canvas.addEventListener("mouseup", VBoard.onPointerUp);
-		canvas.addEventListener("mousemove", VBoard.onPointerMove);
-	};
-
-	VBoard.verticalMoveLoop = function (dir) {
-		window.clearTimeout(VBoard.verticalMoveTick);
-
-		if(dir != 0) {
-			VBoard.camera.position.y += dir*0.05;
-			VBoard.verticalMoveTick = setTimeout(function () {
-				VBoard.verticalMoveLoop(dir);
-			}, 16);
-		}
-	};
-
-	VBoard.horizontalMoveLoop = function (dir) {
-		window.clearTimeout(VBoard.horizontalMoveTick);
-
-		if(dir != 0) {
-			VBoard.camera.position.x += dir*0.05;
-			VBoard.horizontalMoveTick = setTimeout(function () {
-				VBoard.horizontalMoveLoop(dir);
-			}, 16);
-		}
-	};
-
-	VBoard.renderInit = function () {
-		console.log("renderinit");
-		VBoard.frame = 0;
-		VBoard.canvas = document.getElementById("canvas");
-		var canvas = VBoard.canvas;
-		VBoard.engine = new BABYLON.Engine(canvas, true);
-		VBoard.scene = (function () {
-			var scene = new BABYLON.Scene(VBoard.engine);
-			scene.clearColor = new BABYLON.Color3(0.5, 1, 0.984);
-			var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 10, 0), scene);
-			VBoard.camera = camera;
-
-			camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
-			VBoard.setCameraPerspective();
-			camera.setTarget(new BABYLON.Vector3.Zero());
-
-			camera.attachControl(canvas, false);
-			//camera.keysUp.push(87); // W
-			//camera.keysLeft.push(65); // A
-			//camera.keysDown.push(83); // S
-			//camera.keysRight.push(68); // D
-			camera.keysUp = [];
-			camera.keysLeft = [];
-			camera.keysDown = [];
-			camera.keysRight = [];
-			camera.inertia = 0.6;
-			camera.angularSensibility = Infinity;
-			camera.maxCameraSpeed = 80;
-			camera.cameraAcceleration = 0.1;
-			//camera.rotation = new BABYLON.Vector3(Math.PI/2, 0, 0);
-
-			scene.activeCamera = camera;
-			camera.detachControl(canvas);
-			VBoard.light = new BABYLON.HemisphericLight("hemi", new BABYLON.Vector3(0, 1, 0), scene);
-			VBoard.light.groundColor = new BABYLON.Color3(1, 1, 0.984);
-
-			//scene.onDispose = function () {
-			//	window.removeEventListener("pointerdown", VBoard.onPointerDown);
-			//	window.removeEventListener("pointerup", VBoard.onPointerUp);
-			//	window.removeEventListener("pointermove", VBoard.onPointerMove);
-			// };
-
-			return scene;
-		})();
-		VBoard.engine.runRenderLoop(VBoard.renderLoop);
-	};
-
-	VBoard.onPointerDown = function (evt) {
-		//if(evt.button !== 0) {
-		//	return;
-		// }
-		console.log("pointer down");
-
-		var pickInfo = VBoard.scene.pick(VBoard.scene.pointerX, VBoard.scene.pointerY, function (mesh) {
-			return true;
-		});
-
-		if(pickInfo.hit) {
-			VBoard.currentMesh = pickInfo.pickedMesh;
-			VBoard.lastMeshPos = [VBoard.scene.pointerX, VBoard.scene.pointerY];
-		}
-	};
-
-	VBoard.onPointerUp = function (evt) {
-		//if(evt.button !== 0) {
-		//	return;
-		// }
-		console.log("pointer up");
-		VBoard.currentMesh = null;
-	};
-
-	VBoard.onPointerMove = function (evt) {
-		if(!VBoard.currentMesh) {
-			return;
-		}
-		console.log("pointer move");
-		var currPos = [VBoard.scene.pointerX, VBoard.scene.pointerY];
-		var diffx = currPos[0] - VBoard.lastMeshPos[0];
-		var diffy = currPos[1] - VBoard.lastMeshPos[1];
-		VBoard.currentMesh.position = VBoard.currentMesh.position.add(new BABYLON.Vector3(diffx, -diffy, 0));
-		VBoard.lastMeshPos = currPos;
-	};
-
-	VBoard.loadDummyBlocks = function () {
-		var material1 = new BABYLON.StandardMaterial("std", VBoard.scene);
-		material1.diffuseColor = new BABYLON.Color3(1, 1, 1);
-		material1.diffuseTexture = new BABYLON.Texture("b1.png", VBoard.scene);
-
-		var material2 = new BABYLON.StandardMaterial("std", VBoard.scene);
-		//material2.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-		material2.diffuseTexture = new BABYLON.Texture("SPACE_1.jpg", VBoard.scene);
-
-		var material3 = new BABYLON.StandardMaterial("std", VBoard.scene);
-		material3.diffuseTexture = new BABYLON.Texture("crown.png", VBoard.scene);
-		material3.diffuseTexture.hasAlpha = true;
-		//material3.diffuseTexture.useAlphaFromDiffuseTexture = true;
-
-		var plane1 = BABYLON.Mesh.CreatePlane("plane", 3.0, VBoard.scene);
-		plane1.material = material1;
-		plane1.position = new BABYLON.Vector3(0, 10, 1);
-
-		var plane2 = BABYLON.Mesh.CreatePlane("plane", 2.0, VBoard.scene);
-		plane2.material = material3;
-		plane2.position = new BABYLON.Vector3(0, 3, 1.5);
-
-		var plane3 = BABYLON.Mesh.CreatePlane("plane", 4.0, VBoard.scene);
-		plane3.material = material2;
-		plane3.position = new BABYLON.Vector3(6, 4, 2);
-	};
-
-	VBoard.renderLoop = function () {
-		console.log("renderloop");
-		//console.debug(VBoard);
-		if(VBoard.box) {
-			//VBoard.box.position.y = 3*Math.sin(VBoard.frame / 50);
-		}
-		VBoard.frame++;
-		VBoard.scene.render();
-	};
-
-	VBoard.setCameraPerspective = function () {
-		var ratio = window.innerWidth / window.innerHeight;
-		var size = 10;
-		VBoard.camera.orthoTop = size;
-		VBoard.camera.orthoBottom = -size;
-		VBoard.camera.orthoRight = ratio*size;
-		VBoard.camera.orthoLeft = -ratio*size;
-	};
-
-	VBoard.resizeFunc = function () {
-		VBoard.setCameraPerspective();
-		//VBoard.engine.resize();
-	};
-})(VBoard);
-$(document).ready(function () {
-	console.log("document ready");
-	VBoard.javascriptInit();
-});
-*/
