6b2b99798bf8051e4d13175c84205af2c2bec42a
Daniel Pugliese <dpugliese@host-25-75.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-03-04 07:34:20 -0600

    Added baord_state.py

---
+++ virtualboard/web/modules/board_state.py
@@ -0,0 +1,76 @@
+import json
+
+class Piece(object):
+    name = ""
+    x = 0
+    y = 0
+    z = 0
+    user = ""
+    icon = ""
+    piece_id = 0
+
+    def __init__(self, name="", x=0, y=0, z=0, user="", icon="", piece_id=0):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.z = z
+        self.user = user
+        self.icon = icon
+        self.piece_id = piece_id
+
+    def set_name(new_name):
+        name = new_name
+
+    def set_x(new_x):
+        x = new_x
+
+    def set_y(new_y):
+        y = new_y
+
+    def set_z(new_z):
+        z = new_z
+
+    def set_user(new_user):
+        user = new_user
+
+    def set_icon(new_icon):
+        icon = new_icon
+
+    def set_piece_id(new_id):
+        piece_id = new_id
+
+    def get_name():
+        return name
+
+    def get_x():
+        return x
+
+    def get_y():
+        return y
+
+    def get_z():
+        return z
+
+    def get_user():
+        return user
+
+    def get_icon():
+        return icon
+
+    def get_piece_id():
+        return piece_id
+
+class BoardState(object):
+    pieces = set()
+
+    def add_piece(piece):
+        pieces.add(piece)
+
+    def remove_piece(piece):
+        pieces.remove(piece)
+
+    def dump_json():
+        return json.dump(pieces)
+
+    def load_json(json_string):
+        pieces = json.load(json_string)
