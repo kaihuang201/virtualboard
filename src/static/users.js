5ed878a184f7eb1ce990431e22f07b5f95945b31
Daniel Pugliese <dpugliese@host-25-88.ilcujsm.champaign.il.us.clients.pavlovmedia.com>
2016-05-01 23:00:57 -0500

    Added comments and removed some dead code

--- src/static/users.js
+++ src/static/users.js
@@ -21,20 +21,25 @@ var VBoard = VBoard || {};
 		// },
 
 		//methods
+
+		// Adds a user
 		add: function (user) {
 			this.userList[user.id] = user;
 		},
 
+		// Removes a user
 		remove: function (user) {
 			delete this.userList[user.id];
 		},
 
+		// Removes a user based on userData JSON object
 		removeUser: function (userData) {
 			var id = userData["user"];
 			var user = this.userList[id];
 			this.remove(user);
 		},
 
+		// Returns the user with the given id, or null if none exists
 		getFromID: function (id) {
 			if(this.userList.hasOwnProperty(id)) {
 				return this.userList[id];
@@ -42,34 +47,35 @@ var VBoard = VBoard || {};
 			return null;
 		},
 
-		//to do: actual implementation
+		// Sets the given user as the local user
 		setLocal: function (user) {
 			this.local = user;
 		},
 
+		// Returns the local user
 		getLocal: function () {
 			return this.local;
 		},
 
-		getNone: function () {
-			return null;
-		},
-
+		// returns the host
 		getHost: function () {
 			return this.host;
 		},
 
+		// Sets a the color of a user specified by id to the given color
 		changeUserColor: function (userID, colorArr) {
 			var user = this.userList[userID];
 			var bcolor = new BABYLON.Color3(colorArr[0]/255, colorArr[1]/255, colorArr[2]/255);
 			this.changeColor(user, bcolor);
 		},
 
+		// Sets a users color
 		changeColor: function (user, color) {
 			user.color = color;
 			//TODO: maybe other stuff needs updating too
 		},
 
+		// Sets the game's host to the user with the given id
 		changeHost: function (id) {
 			var newHost = this.userList[id];
 			this.userList[this.host.id].isHost = false;
@@ -80,6 +86,7 @@ var VBoard = VBoard || {};
 			//probably not in this function
 		},
 
+		// Creates a new user from the given userData
 		createNewUser: function (userData) {
 			var id = userData["user"];
 			var name = userData["name"];
